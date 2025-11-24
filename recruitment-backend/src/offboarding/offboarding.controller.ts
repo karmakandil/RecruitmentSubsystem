5d96edc0aae4d997f5817dc4c0070cd676c6ae98
karmakandil <karmakandil2@gmail.com>
2025-11-24 15:20:59 +0200

    cloned repo

--- recruitment-backend/src/offboarding/offboarding.controller.ts
+++
@@ -1,9 +0,0 @@
-// recruitment-backend/src/offboarding/offboarding.controller.ts
-
-import { Controller } from '@nestjs/common';
-
-@Controller('offboarding')
-export class OffboardingController {
-  // Intentionally left empty for now.
-  // Endpoints will be added in later phases when needed.
-}
--- recruitment-backend/src/offboarding/offboarding.service.ts
+++
@@ -1,9 +0,0 @@
-// recruitment-backend/src/offboarding/offboarding.service.ts
-
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class OffboardingService {
-  // Intentionally left empty for now.
-  // We are focusing only on schemas for this milestone.
-}
