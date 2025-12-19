1e295fbb84f00bc2ac231ce940204ad820d18cfb
karmakandil <karmakandil2@gmail.com>
2025-12-20 00:25:50 +0200

    final frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
   // -----------------------------------
   // START SERVER
   // -----------------------------------
-  const port = process.env.PORT || 6000;
+  const port = process.env.PORT || 5000;
   await app.listen(port);
 
   console.log('='.repeat(50));
